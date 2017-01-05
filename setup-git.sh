b7b4f18a6ec4b8c49c81a283cd9e38e6548bc535
Nathan Bunrham <Nathan Burnham>
2017-01-05 14:48:34 -0500

    added tmux setup

--- setup-git.sh
+++ setup-git.sh
@@ -1,8 +1,11 @@
 sudo apt-get install -y git ssh
 git config --global user.email "Nathan Burnham"
 git config user.email "nburn42@gmail.com"
-ssh-keygen -t rsa -b 4096 -C "nburn42@gmail.com"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-echo "add follwoing to github (.ssh/id_rsa.pub)"
-echo ~/.ssh/id_rsa.pub
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+#ssh-keygen -t rsa -b 4096 -C "nburn42@gmail.com"
+#eval "$(ssh-agent -s)"
+#ssh-add ~/.ssh/id_rsa
+#echo "add follwoing to github (.ssh/id_rsa.pub)"
+#echo ~/.ssh/id_rsa.pub
