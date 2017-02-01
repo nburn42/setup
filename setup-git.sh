5cf2274b57718a1d5bed20797c765916bc2e64fb
Nathan Burnham <nburn42@gmail.com>
2017-02-01 05:46:49 +0000

    fixed git setup

--- setup-git.sh
+++ setup-git.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y git ssh
-git config --global user.email "Nathan Burnham"
-git config user.email "nburn42@gmail.com"
+git config --global user.name "Nathan Burnham"
+git config --global user.email "nburn42@gmail.com"
 
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
